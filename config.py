c484d183547d52a9f2cf6be5de641ee20a0f4967
kingstar <athenesaint@gmail.com>
2023-07-17 20:49:59 -0400

    first_commit

---
+++ config.py
@@ -0,0 +1,20 @@
+OCTO_API_TOKEN = '59f0c196b2a447449bf0946806b0dc11' # Octobrowser api token
+OCTO_HTTP_SERVER = 58888
+OCTO_ID = 'EUGENE_VISA_TEST'
+OCTO_AUTOMATION_URL = 'https://app.octobrowser.net/api/v2/automation/profiles'
+OCTO_HEADER = {
+    'Content-Type': 'application/json',
+    'X-Octo-Api-Token': OCTO_API_TOKEN
+}
+LOCAL_API = 'http://localhost:58888/api/profiles'
+
+VISA_URL = "https://onlineservices.immigration.govt.nz/?WHS"
+
+# Proxy info
+PROXY = {
+    "type": "socks5",
+    "host": "178.253.215.162",
+    "port": 12324,
+    "login": "14a6f067eab2a",
+    "password": "d4516c8c50"
+}
